f312bc9e7d10303a2ca20f06dc4cea354cbb22c1
Yangfei Li <lyfelmer@outlook.com>
2017-11-28 19:10:09 -0500

    introduction page

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+app.get("/introduction", function(req, res) {
+	review = [
+	{
+		userID:"123";
+		content:"nsaofdnioasf",
+	"sadnlaf",
+	"andsfioa"]
+
+	res.render("introduction", {review:review});
+})
 
 app.listen(3000, function() {
 	console.log("MovieBook Server Start!");
