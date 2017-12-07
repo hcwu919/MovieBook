1ab0001e5356742610b667fa5625659e723f7c1d
Yangfei Li <lyfelmer@outlook.com>
2017-12-07 01:15:49 -0500

    new start

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-	res.render("landing");
+	res.render("homepage");
 });
 
-app.get("/introduction", function(req, res) {
-	review = [
-	{
-		userID:"123";
-		content:"nsaofdnioasf",
-	"sadnlaf",
-	"andsfioa"]
-
-	res.render("introduction", {review:review});
+app.get("/movie", function(req, res) {
+	res.render("movie");
 })
 
 app.listen(3000, function() {
