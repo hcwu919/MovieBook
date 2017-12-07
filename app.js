c90dfecdcf2a07c1ca54acb3578fb8b39b232747
bob <haocheng.wu@hotmail.com>
2017-12-07 02:35:25 -0500

    connect to MySQL on AWS

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'database2017.c7dghyf3qfms.us-west-2.rds.amazonaws.com',
+    user: 'database2017',
+    password: '550database2017',
+    database: 'Movie_RDS'
+});
+
+console.log("connected");
+
+connection.connect();
 
 app.set("view engine", "ejs");
 
@@ -9,8 +20,21 @@ app.get("/", function(req, res) {
 
 app.get("/movie", function(req, res) {
 	res.render("movie");
-})
+});
+
+app.get('/movies/:id', function(req, res) {
+    var mid = req.params.id;
+    console.log(mid);
+
+    var query = "SELECT * FROM Movie WHERE imdbId = " + mid + "";
+    connection.query(query, function (err, movies) {
+        if(err) throw err;
+        console.log(query);
+        var movie = movies[0]["title"];
+        res.render('movieDetails', {movie : movie});
+    })
+});
 
 app.listen(3000, function() {
 	console.log("MovieBook Server Start!");
-})
+});
