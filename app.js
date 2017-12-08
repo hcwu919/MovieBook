ec9eb6f82a25af2b07850fe150e11ece6ef5190d
Yangfei Li <lyfelmer@outlook.com>
2017-12-08 02:40:09 -0500

    aaa

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'database2017.c7dghyf3qfms.us-west-2.rds.amazonaws.com',
@@ -9,13 +10,14 @@ var connection = mysql.createConnection({
 });
 // var http = require('http');
 
-console.log("connected");
+
 
 connection.connect();
 // var server = http.createServer(app);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 // homepage
 app.get("/", function(req, res) {
@@ -41,8 +43,26 @@ app.get('/movie/:id', function(req, res) {
 });
 
 app.get('/login', function (req, res) {
-    res.render("login");
+    var uid = req.params.id;
+    console.log(mid);
+
+    var query = "SELECT * FROM Recommendation WHERE uid = " + uid +
+                "Limit 5";
+    connection.query(query, function (err, movies) {
+        if(err) throw err;
+        console.log(query);
+        res.render('homepage/:id', {movie : movies});
+    })
+
+    res.render("homepage/:id");
 });
+
+// homepage after login with recommendation
+app.get('/homepage/:id', function(req, res) {
+    res.render('homepage/:id')
+});
+
+
 
 app.listen(3000, function() {
 	console.log("MovieBook Server Start!");
