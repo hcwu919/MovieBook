efd7d45381604a39ec34c9b9dee5bd2a1efdec3a
Yangfei Li <lyfelmer@outlook.com>
2017-12-09 19:26:33 -0500

    log in & log out

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 var request = require("request");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'database2017.c7dghyf3qfms.us-west-2.rds.amazonaws.com',
@@ -14,39 +19,38 @@ var connection = mysql.createConnection({
 
 
 connection.connect();
-// var server = http.createServer(app);
+mongoose.connect("mongodb://localhost/users");
+app.use(require("express-session")({
+    secret : "moviebook is the best",
+    resave : false,
+    saveUninitialized : false
+}))
 
+// var server = http.createServer(app);
 app.set('views',__dirname + '/views');
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 // homepage
 app.get("/", function(req, res) {
     res.render("homepage");
 });
 
-// search box
-
+// search page
 app.get('/search', function (req, res) {
-    // var content = req.body.searchContent;
-    // // content = content.split("").join("''");
-    // var type = req.body.searchType;
-    // console.log(type);
-    // console.log(content);
-    // var query;
-    // // if(type=="title") {
-    // query = "SELECT distinct title from Movie WHERE title LIKE '%" + content + "%' LIMIT 50";
-    // connection.query(query, function (err, movies) {
-    //
-    //     if (err) throw err;
-    //     console.log(JSON.stringify(movies));
-    //     // if(!movies){ res.render('404', { isLogin: isLogin }); return; }
-    //     res.render('result', {movies: movies});
-    // });
     res.render('search');
 });
 
+// search result page
 app.post('/result', function(req, res) {
     var content = req.body.searchContent;
     // content = content.split("").join("''");
@@ -66,9 +70,7 @@ app.post('/result', function(req, res) {
     // }
 });
 
-
-
-
+// api search result page
 app.post("/apisearch", function (req, res) {
     var content = req.body.searchContent;
     console.log(content);
@@ -89,11 +91,9 @@ app.post("/apisearch", function (req, res) {
 });
 
 
-
 app.get("/movie", function(req, res) {
     res.render("movie");
 });
-
 
 
 // movie details
@@ -112,23 +112,61 @@ app.get('/movieDetails', function(req, res) {
     // })
 });
 
-app.get('/login', function (req, res) {
-    res.render('login');
-    var uid = req.params.id;
-    console.log(mid);
 
-    var query = "SELECT * FROM Recommendation WHERE uid = " + uid +
-                "Limit 5";
-    connection.query(query, function (err, movies) {
-        if(err) throw err;
-        console.log(query);
-        res.render('homepage/:id', {movie : movies});
+// ======== AUTH ROUT ===========
+// app.get('/secret', function (req, res) {
+//     res.render("secret");
+// });
+
+
+// register page
+app.get('/register', function (req, res) {
+    res.render("register");
+});
+
+// register logic
+app.post('/register', function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/");
+        })
     })
-
-    res.render("homepage");
 });
 
 
+// log in page
+app.get('/login', function (req, res) {
+    console.log("login");
+    res.render("login");
+});
+
+// log in logic
+// middleware
+app.post('/login', passport.authenticate("local",
+    {
+        successRedirect : "/",
+        failureRedirect : "/login"
+    }) ,function (req, res) {
+});
+
+
+// log out
+app.get('logout', function (req, res) {
+    req.logout();
+    res.redirect("/");
+})
+
+function isLoggedIn(res, req, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 app.listen(3000, function() {
